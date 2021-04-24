64f5e113ef33c0e47ce78aec71ec1d027434f60f
Steve <steve973@gmail.com>
2021-04-24 09:06:16 -0400

    Changed git pre-commit exit code.

---
+++ project-resources/git/hooks/pre-commit.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Display a message so that we know that
+# the commit is running:
+echo "Pre-commit hook running..."
+# Take a long time to do nothing:
+sleep 5
+# For this example, just proceed with the commit
+exit 0
---
+++ project-resources/scripts/git-install-hooks.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+PROJECT_BASE_DIR=${1}
+
+# Ensure that the project-resources git pre-commit hook script is executable
+chmod +x ${PROJECT_BASE_DIR}/project-resources/git/hooks/pre-commit.sh
+
+# Install the project-resources git pre-commit hook as a symlink in the git hooks directory
+ln -sfn ${PROJECT_BASE_DIR}/project-resources/git/hooks/pre-commit.sh ${PROJECT_BASE_DIR}/.git/hooks/pre-commit
